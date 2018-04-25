541cbe81c7ddff3e1fbf72dfe28fdea7da2fa03a
student <romanmusiyowskij248@gmail.com>
2018-04-25 17:19:13 +0200

    1-3

---
+++ OP8/src/op8/Ksiazka.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package op8;
+
+/**
+ *
+ * @author student
+ */
+public class Ksiazka extends Product {
+       public Ksiazka(double price, String name, String describe) {
+        super(price, name, describe);
+    }
+    
+    @Override
+    public void buy() {
+        System.out.println(
+                "I bought a black tractor, capaciticy 2400!");
+    }
+    @Override
+    public void showInfo() {
+        System.out.println(
+                "Price:" + getPrice()
+                + " name:" + getName()
+                + " describe:" + getDescribe()
+        );
+    }
+    
+    
+}
